2e786e4f71fe3ddb2bd46b7f3701c551460cadd7
Achraf Chaibi <achraf.chaibi@outmind.fr>
2025-04-07 01:33:58 +0200

    Apply code edit to app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ import { Button } from '@/components/ui/button';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import { FaInstagram, FaFacebookF, FaTwitter } from 'react-icons/fa';
 
-
   export default function HomePage() {
     return (
       <main className="flex flex-col">
